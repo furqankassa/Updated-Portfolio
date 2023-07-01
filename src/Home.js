5a08fd20c112ea21bfed0846fd550a8f93b5954c
Furqan Kassa <furqankassa@Furqans-MacBook-Pro.local>
2023-07-01 17:44:31 -0500

    Migrating to React for portfolio

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <>
+      <p>
+        My name is Furqan Kassa, a senior at the University of
+        Washington, and I'm currently CS with a minor in
+        Mathematics. I'm also interning as a Software Engineer
+        at Capital One.
+      </p>
+    </>
+  );
+}
